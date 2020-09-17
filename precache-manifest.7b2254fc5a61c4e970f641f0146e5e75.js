adc596c5c8238ecd7872c756595725a4f135c8c2
David <davidg5@vt.edu>
2020-09-17 00:57:01 -0400

    Updates

---
+++ precache-manifest.7b2254fc5a61c4e970f641f0146e5e75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d71dd44749ba3e9f7e7178e8c631ff0c",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "fb645dc3637a9d7d756d",
+    "url": "/chat/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc04c3b1243170f018a4",
+    "url": "/chat/static/js/2.1ac19246.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chat/static/js/2.1ac19246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb645dc3637a9d7d756d",
+    "url": "/chat/static/js/main.a24978ef.chunk.js"
+  },
+  {
+    "revision": "8d86be0bcc4812848edd",
+    "url": "/chat/static/js/runtime-main.6b736942.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chat/static/media/logo.5d5d9eef.svg"
+  }
+]);
