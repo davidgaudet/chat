dfa7cff92aa4f1359b6242b15dbc273f49e322e3
David <davidg5@vt.edu>
2020-11-11 19:13:36 -0500

    Starting point for UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import Map from './Map'
+import Top from './Top'
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import './App.css';
 
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+/*
 function App() {
   return (
     <div className="App">
@@ -20,9 +27,113 @@ function App() {
           .
         </a>
       </header>
+      <Button variant="contained">Default</Button>
       <Map />	  
     </div>
   );
 }
+*/
 
+
+function App() {
+  return (
+    <div className="App">
+      <Top />
+      
+      <CenteredGrid />
+    </div>
+  );
+}
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    
+  },
+  roundStatusCard: {
+    backgroundColor:"lightskyblue"
+  },
+  memberStatusCard: {
+    backgroundColor:"mediumpurple"
+  },
+  meetingRowCard: {
+    margin: "6px",
+    backgroundColor:"white"
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+}));
+
+function CenteredGrid() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Grid container spacing={10} direction="column" justify="center" alignItems="center">
+        
+        <Grid container item direction="row" justify="space-around" alignItems="stretch">
+          <Grid item component={Card} className={classes.roundStatusCard}>
+            <CardContent>
+              <Typography variant="body2" component="p">
+                Rounds Completed: 0
+                <br />
+                Rounds Remaining: 0
+              </Typography>
+            </CardContent>
+          </Grid>
+          <Grid item component={Card} className={classes.memberStatusCard}>
+            <CardContent>
+              <Typography variant="body2" component="p">
+                New Members
+              </Typography>
+            </CardContent>
+          </Grid>
+          <Grid item component={Card} className={classes.memberStatusCard}>
+            <CardContent>
+              <Typography variant="body2" component="p">
+                Existing Members
+              </Typography>
+            </CardContent>
+          </Grid>
+        </Grid>
+      
+
+        <Grid container item direction="column" justify="space-evenly" alignItems="center">
+          <Grid item component={Card} className={classes.meetingRowCard}>
+            <CardContent>
+              <Typography variant="body2" component="p">
+                O O O O O O O O O
+              </Typography>
+            </CardContent>
+          </Grid>
+          <Grid item component={Card} className={classes.meetingRowCard}>
+            <CardContent>
+              <Typography variant="body2" component="p">
+                O O O O O O O O O
+              </Typography>
+            </CardContent>
+          </Grid>
+        </Grid>
+      
+        <Grid container item justify="center" alignItems="center">
+          <Grid>
+            <Button variant="contained" style={{backgroundColor: "lightgreen", margin: "10px"}} >
+              Start Round
+            </Button>
+          </Grid>
+        </Grid>
+
+      </Grid>
+    </div>
+  );
+}
 export default App;
---
+++ src/Top.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Top extends React.Component {
+
+	render() {
+		return (
+			<p> * Meeter * </p>
+		);
+	}
+}
+
+export default Top
