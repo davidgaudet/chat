21adf474f34a8217688289835779d2efd4e04837
emily-desverreaux <emilydesverreaux@Emilys-MacBook-Pro.local>
2020-11-15 15:33:05 -0500

    created a pop up modal to add new members

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ import AddIcon from '@material-ui/icons/Add';
 import Fab from '@material-ui/core/Fab';
 import Avatar from '@material-ui/core/Avatar';
 import AvatarGroup from '@material-ui/lab/AvatarGroup';
+import Modal from '@material-ui/core/Modal';
+import TextField from '@material-ui/core/TextField';
 
-//<Grid item xs={6}>
- //       <div style={{background:randomColor()}}>Hooray something is here!</div>
- //     </Grid>
+
 class App extends React.Component {
 
   constructor(props) {
@@ -26,10 +26,19 @@ class App extends React.Component {
       rounds_completed: 0,
       rounds_remaining: 0,
       new_members: [0],
+      new_member_data: [{
+        first:"Sample",
+        last:"User",
+        role:"Intern",
+        forms:"",
+        pic:"https://i.pinimg.com/736x/ae/c4/53/aec453161b2f33ffc6219d8a758307a9.jpg",
+        dateJoined: "date"
+      }],
       ext_members: [1]
     };
   }
-  
+
+
   render() {
     var meetingRows = [];
 
@@ -51,7 +60,7 @@ class App extends React.Component {
         </Grid>
       );
       new_avatars.push(
-        <Avatar>{this.state.new_members[i]}</Avatar>
+        <Avatar alt={this.state.new_member_data[i].first[0]} src={this.state.new_member_data[i].pic}></Avatar>
       );
     }
 
@@ -76,16 +85,10 @@ class App extends React.Component {
             </CardContent>
           </Grid>
           <Grid item xs={3} component={Card} className={useStyles().memberStatusCard} variant="outlined">
-            <CardHeader 
+            <CardHeader
               action={
-                <Fab size="small" onClick={() => { 
-                  let newMembers = this.state.new_members;
-                  let last = newMembers[newMembers.length-1];
-                  newMembers.push(last+2);
-                  this.setState({new_members: newMembers})}
-                }>
-                  <AddIcon/>
-                </Fab>
+
+                  <SimpleModal/>
               }
               title={
                 <Typography variant="body2" component="p">
@@ -93,6 +96,7 @@ class App extends React.Component {
                 </Typography>
               }
             />
+
             <CardContent>
               <AvatarGroup>
                 {new_avatars}
@@ -100,9 +104,9 @@ class App extends React.Component {
             </CardContent>
           </Grid>
           <Grid item xs={3} component={Card} className={useStyles().memberStatusCard} variant="outlined">
-            <CardHeader 
+            <CardHeader
               action={
-                <Fab size="small" onClick={() => { 
+                <Fab size="small" onClick={() => {
                   let extMembers = this.state.ext_members;
                   let last = extMembers[extMembers.length-1];
                   extMembers.push(last+2);
@@ -124,7 +128,7 @@ class App extends React.Component {
             </CardContent>
           </Grid>
         </Grid>
-    
+
         <Grid container spacing={1} direction="column" justify="center" alignItems="center">
           {meetingRows}
         </Grid>
@@ -137,9 +141,172 @@ class App extends React.Component {
             </Button>
           </Grid>
         </Grid>
-    
+
       </div>
     );
+
+    function rand() {
+      return Math.round(Math.random() * 20) - 10;
+    }
+
+    function getModalStyle() {
+      const top = 50 + rand();
+      const left = 50 + rand();
+
+      return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+      };
+    }
+
+    function BasicTextFields() {
+
+      const classes = useStyles();
+      return (
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField required id="standard-required" className="new-member-first" label="Required" helperText="First Name" placeholder="First Name"/>
+          <TextField required  id="standard-required" className="new-member-last" label="Required" helperText="Last Name" placeholder="Last Name"/>
+          <TextField className="new-member-role" id="standard-basic" label="" helperText="Team Role"   placeholder="Team Role"/>
+          <TextField
+          className="new-member-forms"
+              id="standard-full-width"
+              style={{ margin: 8 }}
+              placeholder="Enter Link"
+              helperText="Google Forms Link"
+              fullWidth
+              margin="normal"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <TextField
+            className="new-member-pic"
+                id="standard-full-width"
+                style={{ margin: 8 }}
+                placeholder="Enter Link"
+                helperText="Profile Picture Link"
+                fullWidth
+                margin="normal"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+        </form>
+
+      );
+    }
+
+    function DatePickers() {
+      const classes = useStyles();
+      return (
+        <form className={classes.container} noValidate>
+          <TextField
+          className="new-member-date"
+            id="date"
+            label="Join Date"
+            type="date"
+            defaultValue="yyyy-MM-dd"
+            className={classes.textField}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </form>
+      );
+    }
+
+    const SimpleModal= () => {
+      const classes = useStyles();
+    // getModalStyle is not a pure function, we roll the style only on the first render
+    const [modalStyle] = React.useState(getModalStyle);
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+      setOpen(true);
+
+    };
+
+    const handleClose = () => {
+      setOpen(false);
+    };
+
+    const addNewAndClose = () => {
+
+
+      var first = document.querySelectorAll("#standard-required")[0].value;
+      var lastN = document.querySelectorAll("#standard-required")[1].value;
+      var role = document.getElementById("standard-basic").value;
+      var forms = document.querySelectorAll("#standard-full-width")[0].value;
+      var pic = document.querySelectorAll("#standard-full-width")[1].value;
+      var date = document.getElementById("date").value;
+      if(first.trim() != "" && lastN.trim() != ""){
+       console.log(first, lastN, role, forms, pic, date);
+       if(pic.trim() == ""){
+         pic = "invalid";
+       }
+       var newMemberToAdd = {
+         "first":first,
+         "last":lastN,
+         "role":role,
+         "forms":forms,
+         "pic":pic,
+         "dateJoined": date
+       };
+       let newMembers = this.state.new_members;
+         let last = newMembers[newMembers.length-1];
+          newMembers.push(last+2);
+          this.setState({new_members: newMembers});
+
+          let memberData = this.state.new_member_data;
+          memberData.push(newMemberToAdd);
+          this.setState({new_member_data: memberData});
+              setOpen(false);
+        }
+        else{
+          if(first.trim() == "" && lastN.trim() == "")
+            alert("First and last name required");
+          else if(first.trim() == "")
+              alert("First name required");
+          else if(lastN.trim() == "")
+                alert("Last name required");
+        }
+
+    };
+
+    const body = (
+      <div style={modalStyle} className={classes.paper}>
+        <h2 id="simple-modal-title">New Teammember Form</h2>
+        <p id="simple-modal-description">
+          Onboard your new member to meeter.
+        </p>
+        <BasicTextFields/>
+        <DatePickers/>
+        <button type="button" onClick={addNewAndClose}>
+        Add User
+        </button>
+
+      </div>
+    );
+      return(
+        <div>
+        <Fab size="small" onClick={() => {handleOpen();
+
+        }
+        }>
+          <AddIcon/>
+        </Fab>
+        <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description">
+      {body}
+    </Modal>
+        </div>
+      );
+    }
+
 
     return(
       <div className="App">
@@ -152,7 +319,7 @@ class App extends React.Component {
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    
+
   },
   roundStatusCard: {
     backgroundColor:"#8DC6F6",
@@ -166,7 +333,50 @@ const useStyles = makeStyles((theme) => ({
   meetingRowCard: {
     border: "1px solid black",
     backgroundColor:"white"
-  }
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  paper: {
+    position: 'absolute',
+    width: 500,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    }
+  },
+  rootles: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  container: {
+   display: 'flex',
+   flexWrap: 'wrap',
+ },
+ textField: {
+   marginLeft: theme.spacing(1),
+   marginRight: theme.spacing(1),
+   width: 200,
+ }
 }));
+
+
+
 
 export default App;
